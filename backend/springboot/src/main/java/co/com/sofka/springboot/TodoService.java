7b0e3fb883624d7ff1971b92f0f98fdbe899b035
federzvz <85709865+federzvz@users.noreply.github.com>
2022-02-23 18:36:15 -0300

    FRONTEND: Insertar y Listar los objetos

--- backend/springboot/src/main/java/co/com/sofka/springboot/TodoService.java
+++ backend/springboot/src/main/java/co/com/sofka/springboot/TodoService.java
@@ -9,36 +9,26 @@ public class TodoService {
     @Autowired
     private TodoRepository repository;
 
-    @GetMapping(value = "api/todos")
     public Iterable<Todo> list(){
         return repository.findAll();
     }
 
-    @PostMapping(value = "api/todo")
     public Todo save(@RequestBody Todo todo){
         return repository.save(todo);
     }
 
-    @PutMapping(value = "api/todo")
     public Todo update(@RequestBody Todo todo){
         try{
             return repository.save(todo);
         }catch (Exception e){
             throw new RuntimeException("No existe el ID para actualizar.");
         }
-
-
-
-
-
     }
 
-    @DeleteMapping(value = "api/{id}/todo")
     public void delete(@PathVariable("id") long id){
         repository.delete(this.get(id));
     }
 
-    @GetMapping(value = "api/{id}/todo")
     public Todo get(@PathVariable("id") long id){
         return repository.findById(id).orElseThrow();
     }
--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useReducer, useEffect, useRef, useState } from 'react';
+
+const HOST_API = "http://localhost:8080/api";
+
+const initialState = {
+  list: [],
+};
+
+const Store = createContext(initialState);
+
+const Form = () =>{
+  const formRef = useRef(null);
+  const { dispatch} = useContext(Store);
+  const [state, setState] = useState({});
+
+  const onAdd = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: null,
+      completed: false
+    };
+
+
+    fetch(HOST_API + "/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  }
+
+  return (<form ref={formRef}>
+    <input type="text" name="name" onChange={(event) => {
+      setState({...state, name: event.target.value})
+    }} ></input>
+    <button onClick={onAdd}>Agregar</button>
+  </form>)
+}
+
+const List = () => {
+  const { dispatch, state } = useContext(Store);
+
+  useEffect(() => {
+    fetch(HOST_API + "/todos")
+      .then(response => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list })
+      })
+  }, [state.list.length,dispatch]);
+  return(
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Nombre</td>
+          <td>¿Esta Completado?</td>
+        </tr>
+      </thead>
+      <tbody>
+      {state.list.map((todo)=> {
+        return <tr key={todo.id}>
+        <td>{todo.id}</td>
+        <td>{todo.name}</td>
+        <td>{todo.isCompleted}</td>
+        </tr>
+      })}
+      </tbody>
+    </table>
+  </div>)
+};
+
+function reducer(state, action) {
+  switch (action.stype) {
+    case "update-list":
+      return { ...state, list: action.list };
+    case "add-item":
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList };
+    default:
+      return state;
+  }
+}
+
+const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<StoreProvider>
+    <h3>To-Do List</h3>
+    <Form />
+    <List />
+  </StoreProvider>)
 }
+
+
 
 export default App;
